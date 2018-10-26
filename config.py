b7a08a8a9a5fa52416b1bba0350addcdbf649fd2
dangorlov <dangorlov@yandex.ru>
2018-10-27 01:02:44 +0300

    FCM_0.1

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Configuration(object):
     SESSION_COOKIE_HTTPONLY = False
     TEMPLATES_AUTO_RELOAD = True
     #SQLALCHEMY_DATABASE_URI =
-    SQLALCHEMY_DATABASE_URI = "sqlite:///b.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///FrontDB.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     #SQLALCHEMY_POOL_SIZE = 100
     #SQLALCHEMY_POOL_RECYCLE = 280
 
-BACK_URL = 'http://alphase.ru:8121'
+BACK_URL = 'http://solarfind.net:8121'
