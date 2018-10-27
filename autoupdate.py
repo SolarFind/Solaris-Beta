0557bd5f32deecfbf5a43825e85e4c61a9857f0b
dangorlov <dangorlov@yandex.ru>
2018-10-27 14:51:45 +0300

    AUP START

---
+++ autoupdate.py
@@ -0,0 +1,10 @@
+from flask import *
+from os import environ
+app = Flask(__name__)
+
+@app.route("/gh")
+def gh():
+    print(request.data)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", environ["AUP_PORT"])
