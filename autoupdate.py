9cbb2ad51b5ef3994d7022a322226d55714a1203
dangorlov <dangorlov@yandex.ru>
2018-10-27 14:59:11 +0300

    AUP HotFix 2

--- autoupdate.py
+++ autoupdate.py
@@ -1,10 +1,12 @@
 from flask import *
+from pprint import pprint
 from os import environ
 app = Flask(__name__)
 
 @app.route("/gh", methods=["POST"])
 def gh():
-    print(request.data)
+    pprint(request.data)
+    return "OK"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", environ["AUP_PORT"])
